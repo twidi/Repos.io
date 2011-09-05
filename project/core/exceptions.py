d27384cbac6672c5e6beb213f1dca08f4cda8b36
twidi <s.angel@twidi.com>
2011-09-05 23:00:31 +0200

    add fetch_allowed and fetch_related_allowed and update ways status is checked

--- project/core/exceptions.py
+++ project/core/exceptions.py
@@ -1,10 +1,6 @@
 class CoreException(Exception):
     pass
 
-class SaveForbiddenInBackend(CoreException):
-    def __init__(self, message):
-        super(SaveForbiddenInBackend, self).__init__(message or 'You cannot save this object in the backend')
-
 class InvalidIdentifiersForProject(CoreException):
     def __init__(self, backend, message=None):
         super(InvalidIdentifiersForProject, self).__init__(
@@ -17,7 +13,7 @@ class InvalidIdentifiersForProject(CoreException):
 
 class OriginalProviderLoginMissing(CoreException):
     def __init__(self, user, backend_name, message=None):
-        super(SaveForbiddenInBackend, self).__init__(
+        super(OriginalProviderLoginMissing, self).__init__(
             message or 'The original_login from the %s backend for the user %s is required' %
             (
                 backend_name,
