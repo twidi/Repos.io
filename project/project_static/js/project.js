5efbb61f64b03d12f91bb1ce2474d79043cd3673
twidi <s.angel@twidi.com>
2011-09-06 03:20:48 +0200

    style messages

--- project/project_static/js/project.js
+++ project/project_static/js/project.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
-  $("body").bind("click", function (e) {
-    $('.dropdown-toggle, .menu').parent("li").removeClass("open");
-  });
-  $(".dropdown-toggle, .menu").click(function (e) {
-    var $li = $(this).parent("li").toggleClass('open');
-    return false;
-  });
+    $("body").bind("click", function (e) {
+        $('.dropdown-toggle, .menu').parent("li").removeClass("open");
+    });
+    $(".dropdown-toggle, .menu").click(function (e) {
+        var $li = $(this).parent("li").toggleClass('open');
+        return false;
+    });
+    $('.alert-message .close').click(function(e) {
+        $(this).parent().hide()
+        e.preventDefault();
+    });
 
 });
