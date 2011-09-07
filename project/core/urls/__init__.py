e5cb358dbade3780188059cabe2b858c726f4723
twidi <s.angel@twidi.com>
2011-09-07 04:14:04 +0200

    error in urls for account with "-", "." or "@"

--- project/core/urls/__init__.py
+++ project/core/urls/__init__.py
@@ -9,6 +9,6 @@ backend_part = r'(?P<backend>(?:%s))/' % '|'.join(BACKENDS.keys())
 urlpatterns = patterns('',
     url(r'^fetch/$', fetch, name='fetch'),
     url(r'^project/' + backend_part + '(?P<project>[\w\-\.]+(?:/[\w\-\.]+)?)/', include('core.urls.repositories')),
-    url(r'^user/' + backend_part + '(?P<slug>\w+\-\.@)/', include('core.urls.accounts')),
+    url(r'^user/' + backend_part + '(?P<slug>[\w\-\.@]+)/', include('core.urls.accounts')),
     url(r'^(?P<identifier>[\w\/\-]+)', default, name='default'),
 )
