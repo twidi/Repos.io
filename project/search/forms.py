8f905c0a7fc915b70ce8da5fb614dcf8acef3cb8
twidi <s.angel@twidi.com>
2011-09-10 17:04:14 +0200

    missing comments in search forms

--- project/search/forms.py
+++ project/search/forms.py
@@ -5,6 +5,9 @@ from core.models import Repository, Account
 class RepositorySearchForm(BaseSearchForm):
 
     def search(self):
+        """
+        Limit to the Repository model
+        """
         sqs = super(RepositorySearchForm, self).search()
         return sqs.models(Repository)
 
@@ -12,5 +15,8 @@ class RepositorySearchForm(BaseSearchForm):
 class AccountSearchForm(BaseSearchForm):
 
     def search(self):
+        """
+        Limit to the Accout model
+        """
         sqs = super(AccountSearchForm, self).search()
         return sqs.models(Account)
