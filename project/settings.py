3698c38fad14d8e49b5c80cb2bb261cce550fa83
twidi <s.angel@twidi.com>
2011-10-31 02:57:19 +0100

    move the templates-cache directory (easier to search in real templates)

--- project/settings.py
+++ project/settings.py
@@ -105,7 +105,7 @@ TEMPLATE_LOADERS = (
 )
 MEDIA_CACHE_DIR = os.path.normpath(os.path.join(MEDIA_ROOT, 'cache/'))
 MEDIA_CACHE_URL = os.path.normpath(os.path.join(MEDIA_URL, 'cache/'))
-TEMPLATE_CACHE_DIR = os.path.normpath(os.path.join(PROJECT_PATH, 'templates-cache/'))
+TEMPLATE_CACHE_DIR = os.path.normpath(os.path.join(PROJECT_PATH, '..', 'templates-cache/'))
 # Enabled modules of the template preprocessor
 TEMPLATE_PREPROCESSOR_OPTIONS = {
         # Default settings
