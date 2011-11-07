befba07d779bd9323e3091bdefcd608308ded811
twidi <s.angel@twidi.com>
2011-11-07 01:31:21 +0100

    do not save searches coming from crawlers

--- project/search/views.py
+++ project/search/views.py
@@ -5,6 +5,7 @@ from haystack.query import SQ, SearchQuerySet, EmptySearchQuerySet
 from pure_pagination import Paginator, InvalidPage
 from saved_searches.views import SavedSearchView as BaseSearchView
 from saved_searches.models import SavedSearch
+from browsecap.browser import is_crawler
 
 from core.models import Account, Repository
 from utils.sort import prepare_sort
@@ -207,12 +208,18 @@ class RepositorySearchView(CoreSearchView):
 
     def save_search(self, page):
         """
-        Do not save if the sort order is not the default one or if a filter is applied
+        Do not save if the sort order is not the default one, if a filter is
+        applied or if it's a crawler
         """
+        # do not save if a filter is applied
         if self.request.GET.get('show-forks', False) == 'y':
             return
+        # do not save if the sort order is not the default one
         sort = self.get_sort()
         if sort and sort['db_sort']:
+            return
+        # do not save if the request is from a user agent
+        if is_crawler(self.request.META.get('HTTP_USER_AGENT', '')):
             return
         return super(RepositorySearchView, self).save_search(page)
 
