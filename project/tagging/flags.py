e107b977da207ab820fc7b91f0c7d5d5c5bc96a8
twidi <s.angel@twidi.com>
2011-11-11 03:13:42 +0100

    Always sort flags the same way

--- project/tagging/flags.py
+++ project/tagging/flags.py
@@ -11,23 +11,26 @@ def split_tags_and_flags(tags, tags_are_dict=False):
     """
     final_tags = []
     special_tags = list(FLAGS)
-    used_special_tags = []
+    used_special_tags = {}
     for tag in tags:
         if tags_are_dict:
             lower_tag = tag['name'].lower()
         else:
             lower_tag = tag.name.lower()
         if lower_tag in FLAGS:
-            used_special_tags.append(tag)
+            used_special_tags[lower_tag] = tag
             special_tags.remove(lower_tag)
         else:
             final_tags.append(tag)
 
     special_tags = [dict(slug=slugify(tag), name=tag) for tag in special_tags]
 
+    sorted_used_special_tags = [used_special_tags[tag] for tag in FLAGS if tag in used_special_tags]
+
+
     return dict(
         special = special_tags,
-        special_used = used_special_tags,
+        special_used = sorted_used_special_tags,
         normal = final_tags
     )
 
