91372e8842b0cecb6488d6ee8b1f89a81e47168f
twidi <s.angel@twidi.com>
2012-05-17 22:29:41 +0200

    cf #61 - Use Github api v3
    Done because the v2 is closing at the end of the mont.
    Done by using pygithub3 instead of python-github2 and adapting calls.
    All works the same.
    Btw, the repository_readme method will make less api calls.

--- project/core/backends/github.py
+++ project/core/backends/github.py
@@ -1,14 +1,17 @@
 # Repos.io / Copyright Stephane Angel / Creative Commons BY-NC-SA license
 
 from copy import copy
+import base64
 
-from github2.client import Github
-from github2.users import User
-from github2.request import HttpError
+from pygithub3 import Github
+from pygithub3.services.repos import Repo
+from pygithub3.exceptions import NotFound
+from requests.exceptions import HTTPError
 
 from django.conf import settings
 
 from core.backends import BaseBackend, README_NAMES, README_TYPES
+
 
 class GithubBackend(BaseBackend):
 
@@ -48,20 +51,20 @@ class GithubBackend(BaseBackend):
         """
         Return an internal exception (BackendError)
         """
-        if isinstance(exception, HttpError):
-            return self.get_exception(exception.code, what)
-        else:
-            return self.get_exception(None, what)
+        code = None
+        if isinstance(exception, HTTPError):
+            code = exception.response.status_code
+        elif isinstance(exception, NotFound):
+            code = 404
+        return self.get_exception(code, what)
 
     def create_github_instance(self, *args, **kwargs):
         """
         Create a Github instance from the given parameters.
         Add, if not provided, the `requests_per_second` and `cache` ones.
         """
-        kwargs.setdefault('requests_per_second', 1)
-        # for debugging, only if set
-        if getattr(settings, 'GITHUB_CACHE_DIR', None):
-            kwargs.setdefault('cache', settings.GITHUB_CACHE_DIR)
+        if 'per_page' not in kwargs:
+            kwargs['per_page'] = 100
         return Github(*args, **kwargs)
 
     def github(self, token=None):
@@ -75,60 +78,9 @@ class GithubBackend(BaseBackend):
         if str_token not in self._github_instances:
             params = {}
             if token:
-                params['access_token'] = token.token
+                params['token'] = token.token
             self._github_instances[str_token] = self.create_github_instance(**params)
         return self._github_instances[str_token]
-
-    def get_result_list(self, method, arguments, error, allow_pages=False):
-        """
-        Try to retrieve all the result from the given `method` with some
-        `arguments`. If `allow_pages` if True, try to fetch all available pages.
-        """
-        # store all data from all pages
-        result = []
-        # start with page one
-        page = 1
-        # we don't know yet the max length of a page
-        max_length = 0
-        while True:
-            try:
-                # get data for the current page
-                if allow_pages:
-                    args = {'page': page }
-                else:
-                    args = {}
-                args.update(arguments)
-                page_result = method(**args)
-
-                # no result ? it's the end
-                if not page_result:
-                    break
-
-                # save the result
-                result += page_result
-
-                # stop here if we don't want to fetch more pages
-                if not allow_pages:
-                    break
-
-                # check length of result
-                l_page = len(page_result)
-                # if smaller than max, it's the last page
-                if l_page < max_length:
-                    break
-                # if bigger, it's the new max
-                if l_page > max_length:
-                    max_length = l_page
-                # go next page
-                page += 1
-
-            except Exception, e:
-                if allow_pages and page > 1 and getattr(e, 'code', None) == 404:
-                    break
-                else:
-                    raise self._get_exception(e, error)
-
-        return result
 
     def user_fetch(self, account, token=None):
         """
@@ -139,7 +91,7 @@ class GithubBackend(BaseBackend):
 
         # get user data fromgithub
         try:
-            guser = github.users.show(account.slug)
+            guser = github.users.get(account.slug)
         except Exception, e:
             raise self._get_exception(e, '%s' % account.slug)
 
@@ -161,8 +113,9 @@ class GithubBackend(BaseBackend):
             homepage = 'blog',
             avatar = 'avatar_url',
             official_created = 'created_at',
-            official_followers_count = 'followers_count',
-            official_following_count = 'following_count',
+            official_followers_count = 'followers',
+            official_following_count = 'following',
+            url = 'html_url',
         )
 
         result = {}
@@ -175,10 +128,10 @@ class GithubBackend(BaseBackend):
         if 'avatar' not in result and getattr(user, 'gravatar_id', None):
                 result['avatar'] = 'http://www.gravatar.com/avatar/%s' % user.gravatar_id
 
-        result['url'] = 'https://github.com/%s/' % user.login
+        if 'url' not in result:
+            result['url'] = 'https://github.com/%s/' % user.login
 
         return result
-
 
     def user_following(self, account, token=None):
         """
@@ -188,12 +141,12 @@ class GithubBackend(BaseBackend):
         github = self.github(token)
 
         # get users data from github
-        gusers = self.get_result_list(github.users.following, dict(username=account.slug), '%s\'s following' % account.slug)
-
         result = []
-
-        for guser in gusers:
-            result.append(self.user_map(User(login=guser)))
+        try:
+            for guser in github.users.followers.list_following(account.slug).iterator():
+                result.append(self.user_map(guser))
+        except Exception, e:
+            raise self._get_exception(e, '%s\'s following' % account.slug)
 
         return result
 
@@ -205,13 +158,12 @@ class GithubBackend(BaseBackend):
         github = self.github(token)
 
         # get users data from github
-        gusers = self.get_result_list(github.users.followers, dict(username=account.slug), '%s\'s followers' % account.slug)
-
         result = []
-
-        # make a dict for each
-        for guser in gusers:
-            result.append(self.user_map(User(login=guser)))
+        try:
+            for guser in github.users.followers.list(account.slug).iterator():
+                result.append(self.user_map(guser))
+        except Exception, e:
+            raise self._get_exception(e, '%s\'s followers' % account.slug)
 
         return result
 
@@ -223,13 +175,12 @@ class GithubBackend(BaseBackend):
         github = self.github(token)
 
         # get repositories data from github
-        grepos = self.get_result_list(github.repos.watching, dict(for_user=account.slug), '%s\'s repositories' % account.slug, allow_pages=True)
-
         result = []
-
-        # make a dict for each
-        for grepo in grepos:
-            result.append(self.repository_map(grepo))
+        try:
+            for grepo in github.repos.watchers.list_repos(account.slug).iterator():
+                result.append(self.repository_map(grepo))
+        except Exception, e:
+            raise self._get_exception(e, '%s\'s repositories' % account.slug)
 
         return result
 
@@ -242,10 +193,10 @@ class GithubBackend(BaseBackend):
                 # a mapped dict
                 owner = repository['official_owner']
                 slug = repository['slug']
-            else:
-                # an original dict from the github api
-                owner = repository['owner']
-                slug = repository['name']
+        elif isinstance(repository, Repo):
+            # a repository from pygithub3
+            owner = repository.owner.login
+            slug = repository.name
         else:
             # a Repository object (from core.models)
             if repository.owner_id:
@@ -253,7 +204,7 @@ class GithubBackend(BaseBackend):
             else:
                 owner = repository.official_owner
             slug = repository.slug
-        return self.github().project_for_user_repo(owner, slug)
+        return '/'.join([owner, slug])
 
     def parse_project(self, project):
         """
@@ -272,10 +223,11 @@ class GithubBackend(BaseBackend):
 
         # get repository data fromgithub
         project = repository.get_project()
+        project_parts = self.parse_project(project)
         try:
-            grepo = github.repos.show(project)
+            grepo = github.repos.get(project_parts['official_owner'], project_parts['slug'])
         except Exception, e:
-            raise self._get_exception(e, '%s' % repository.project)
+            raise self._get_exception(e, '%s' % project)
 
         # associate github repo to core one
         rmap = self.repository_map(grepo)
@@ -293,12 +245,12 @@ class GithubBackend(BaseBackend):
         simple_mapping = dict(
             slug = 'name',
             name = 'name',
-            url = 'url',
+            url = 'html_url',
             description = 'description',
             homepage = 'homepage',
-            official_owner = 'owner',
+            official_owner = 'owner',  # WARNING : It's an object !
             official_forks_count = 'forks',
-            official_fork_of = 'parent',
+            official_fork_of = 'parent',  # WARNING : It's an object !
             official_followers_count = 'watchers',
             is_fork = 'fork',
             private = 'private',
@@ -314,6 +266,11 @@ class GithubBackend(BaseBackend):
             if value is not None:
                 result[internal_key] = value
 
+        if 'official_owner' in result:
+            result['official_owner'] = result['official_owner'].login
+        if 'official_fork_of' in result:
+            result['official_fork_of'] = self.repository_project(result['official_fork_of'])
+
         result['project'] = self.repository_project(result)
 
         return result
@@ -326,13 +283,14 @@ class GithubBackend(BaseBackend):
         github = self.github(token)
 
         # get users data from github
-        gusers = self.get_result_list(github.repos.watchers, dict(project=repository.project), '%s\'s followers' % repository.project)
-
+        project = repository.get_project()
+        project_parts = self.parse_project(project)
         result = []
-
-        # make a dict for each
-        for guser in gusers:
-            result.append(self.user_map(User(login=guser)))
+        try:
+            for guser in github.repos.watchers.list(project_parts['official_owner'], project_parts['slug']).iterator():
+                result.append(self.user_map(guser))
+        except Exception, e:
+            raise self._get_exception(e, '%s\'s followers' % project)
 
         return result
 
@@ -346,17 +304,17 @@ class GithubBackend(BaseBackend):
         github = self.github(token)
 
         # get users data from github
-        gusers = self.get_result_list(github.repos.list_contributors, dict(project=repository.project), '%s\'s contributors' % repository.project)
-
+        project = repository.get_project()
+        project_parts = self.parse_project(project)
         result = []
-
-        # make a dict for each
-        for guser in gusers:
-            if guser != 'invalid-email-address':
+        try:
+            for guser in github.repos.list_contributors(project_parts['official_owner'], project_parts['slug']).iterator():
                 account_dict = self.user_map(guser)
                 # TODO : nb of contributions not used yet but later...
                 account_dict.setdefault('__extra__', {})['contributions'] = guser.contributions
                 result.append(account_dict)
+        except Exception, e:
+            raise self._get_exception(e, '%s\'s followers' % project)
 
         return result
 
@@ -367,82 +325,73 @@ class GithubBackend(BaseBackend):
         # get/create the github instance
         github = self.github(token)
 
-        # try with each name
-        commits = None
-        for name in README_NAMES:
-            # we start by getting the last commit id for a file starting with this name
+        project = repository.get_project()
+        project_parts = self.parse_project(project)
+
+        empty_readme = ('', None)
+
+        # get all files at the root of the project
+        try:
+            tree = github.git_data.trees.get(
+                sha = repository.default_branch or 'master',
+                recursive = None,
+                user = project_parts['official_owner'],
+                repo = project_parts['slug'],
+            )
+        except NotFound:
+            return empty_readme
+        except Exception, e:
+            raise self._get_exception(e, '%s\'s readme' % project)
+
+        # filter readme files
+        files = [f for f in tree.tree if f.get('type', None) == 'blob'
+            and 'path' in f and any(f['path'].startswith(n) for n in README_NAMES)]
+
+        # not readme file found, exit
+        if not files:
+            return empty_readme
+
+        # get contents for all these files
+        contents = []
+        for fil in files:
+            filename = fil['path']
             try:
-                commits = github.commits.list(
-                    project = repository.project,
-                    branch = repository.default_branch or 'master',
-                    file = '%s*' % name
+                blob = github.git_data.blobs.get(
+                    sha = fil['sha'],
+                    user = project_parts['official_owner'],
+                    repo = project_parts['slug'],
                 )
-            except HttpError, e:
-                if e.code == 404:
-                    continue
-                raise self._get_exception(e, '%s\'s commits' % repository.project)
+            except NotFound:
+                continue
             except Exception, e:
-                raise self._get_exception(e, '%s\'s commits' % repository.project)
+                raise self._get_exception(e, '%s\'s readme file' % repository.project)
             else:
-                found_name = name
-                break
-
-        # no commit found => no readme
-        if not commits:
-            return ('', None)
-
-        for commit in commits:
-
-            # get more information about this commit
-            try:
-                commit_infos = github.commits.show(repository.project, commits[0].id)
-            except Exception, e:
-                raise self._get_exception(e, '%s\'s commits' % repository.project)
-
-            # nothing added or modified ?
-            if not commit_infos.added and not commit_infos.modified:
-                continue
-
-            # get files that looks like a readme file
-            files = set()
-            if commit_infos.added:
-                files.update([file for file in commit_infos.added if file.startswith(found_name)])
-            if commit_infos.modified:
-                files.update([diff['filename'] for diff in commit_infos.modified if diff['filename'].startswith(found_name)])
-
-            if not files:
-                continue
-
-            # get contents for all these files
-            contents = []
-            for filename in files:
                 try:
-                    blob = github.get_blob_info(repository.project, commit.id, filename)
-                except Exception, e:
-                    raise self._get_exception(e, '%s\'s readme file' % repository.project)
-                else:
-                    contents.append((filename, blob.get('data', 0)))
+                    content = blob.content
+                    if blob.encoding == 'base64':
+                        content = base64.decodestring(content)
+                    contents.append((filename, content))
+                except:
+                    return empty_readme
 
-            if not contents:
-                continue
+        if not contents:
+            return empty_readme
 
-            # keep the biggest
-            filename, content = sorted(contents, key=len)[-1]
+        # keep the biggest
+        filename, content = sorted(contents, key=len)[-1]
 
-            # find the type
-            filetype = 'txt'
-            try:
-                extension = filename.split('.')[-1]
-                for ftype, extensions in README_TYPES:
-                    if extension in extensions:
-                        filetype = ftype
-                        break
-            except:
-                pass
+        # find the type
+        filetype = 'txt'
+        try:
+            extension = filename.split('.')[-1]
+            for ftype, extensions in README_TYPES:
+                if extension in extensions:
+                    filetype = ftype
+                    break
+        except:
+            pass
 
-            return content, filetype
-
-        return ('', None)
+        return content, filetype
 
 
-BACKENDS = { 'github': GithubBackend, }
+BACKENDS = {'github': GithubBackend, }
